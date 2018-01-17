8cf3ed3e78a0cad1b19b49e89904acd5f60d80d9
Laurynas <laurynaslavre@gmail.com>
2018-01-17 18:28:55 +0200

    added webpack alias for scss folder and imported to global vue

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'helpers': resolve('src/scss/helpers')
     }
   },
   module: {
--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,7 @@ export default {
 </script>
 
 <style lang="scss">
-
+@import '~helpers/reset';
+@import '~helpers/variables';
+@import '~helpers/mixins';
 </style>
