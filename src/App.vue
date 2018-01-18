92552ef83a25abefbf2261f94e78bb44a564c1a3
Laurynas <laurynaslavre@gmail.com>
2018-01-18 19:43:48 +0200

    test modal with custom form and small fixes

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,10 @@ export default {
 @import '~helpers/reset';
 @import '~helpers/variables';
 
+.line.el-col {
+  text-align: center;
+}
+
 .el-message {
   font-family: $font-primary;
   .el-message__content {
@@ -56,6 +60,63 @@ export default {
   .el-loading-text {
     color: darken($gray, 25);
   }
+}
+
+.el-dialog__headerbtn:focus,
+.el-dialog__close,
+.el-dialog__headerbtn:hover .el-dialog__close {
+  color: $purple;
+}
+
+.el-button {
+  border: 0;
+  color: darken($gray, 25);
+  &:hover,
+  &:focus {
+    color: $purple;
+    border-color: $purple;
+    background-color: rgba($purple, .1);
+  }
+}
+
+
+.el-button--primary {
+  background: $purple;
+  color: white;
+  &:hover {
+    color: white;
+    background: lighten($purple, 5);
+  }
+  &:active {
+    background: darken($purple, 5);
+  }
+  &:focus {
+    background: $purple;
+  }
+}
+.el-button--primary:focus, .el-button--primary:hover
+
+.el-switch.is-checked {
+  .el-switch__core {
+    background: lighten($purple, 5);
+    border-color: $purple;
+  }
+}
+
+.el-textarea__inner,
+.el-input__inner {
+  &:focus {
+    border-color: $purple;
+  }
+}
+
+.v-modal {
+  background: $purple;
+  opacity: .9;
+}
+
+.el-dialog {
+  box-shadow: 0 0 41px 0 rgba(darken($gray, 50),.12);
 }
 
 .aig__app {
--- src/views/Data.vue
+++ src/views/Data.vue
@@ -11,12 +11,47 @@
       </div>
       <div style="margin-top: 30px;">
         <Button color="purple" @click="modalUpload = true">Upload new data</Button>
-        <el-dialog title="Upload new data" :visible.sync="modalUpload" width="30%">
-          <span>This is a message</span>
-          <span slot="footer" class="dialog-footer">
-            <Button @click="modalUpload = false">Cancel</Button>
-            <Button color="purple" @click="modalUpload = false">Upload</Button>
-          </span>
+        <el-dialog title="Upload new data" :visible.sync="modalUpload" width="560px">
+          <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
+            <el-form-item label="Activity name" prop="name">
+              <el-input v-model="ruleForm.name"></el-input>
+            </el-form-item>
+            <el-form-item label="Activity zone" prop="region">
+              <el-select v-model="ruleForm.region" placeholder="Activity zone">
+                <el-option label="Zone one" value="shanghai"></el-option>
+                <el-option label="Zone two" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="Activity time" required>
+              <el-col :span="11">
+                <el-form-item prop="date1">
+                  <el-date-picker type="date" placeholder="Pick a date" v-model="ruleForm.date1" style="width: 100%;"></el-date-picker>
+                </el-form-item>
+              </el-col>
+              <el-col class="line" :span="2">-</el-col>
+              <el-col :span="11">
+                <el-form-item prop="date2">
+                  <el-time-picker type="fixed-time" placeholder="Pick a time" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
+                </el-form-item>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="Instant delivery" prop="delivery">
+              <el-switch v-model="ruleForm.delivery"></el-switch>
+            </el-form-item>
+            <el-form-item label="Resources" prop="resource">
+              <el-radio-group v-model="ruleForm.resource">
+                <el-radio label="Sponsorship"></el-radio>
+                <el-radio label="Venue"></el-radio>
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item label="Activity form" prop="desc">
+              <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('ruleForm')">Create</el-button>
+              <el-button @click="resetForm('ruleForm')">Reset</el-button>
+            </el-form-item>
+          </el-form>
         </el-dialog>
       </div>
     </div>
@@ -36,7 +71,41 @@ export default {
   data () {
     return {
       modalUpload: false,
-      msg: 'Data view'
+      msg: 'Data view',
+      ruleForm: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: 'Please input Activity name', trigger: 'blur' },
+          { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' }
+        ],
+        region: [
+          { required: true, message: 'Please select Activity zone', trigger: 'change' }
+        ],
+        date1: [
+          { type: 'date', required: true, message: 'Please pick a date', trigger: 'change' }
+        ],
+        date2: [
+          { type: 'date', required: true, message: 'Please pick a time', trigger: 'change' }
+        ],
+        type: [
+          { type: 'array', required: true, message: 'Please select at least one activity type', trigger: 'change' }
+        ],
+        resource: [
+          { required: true, message: 'Please select activity resource', trigger: 'change' }
+        ],
+        desc: [
+          { required: true, message: 'Please input activity form', trigger: 'blur' }
+        ]
+      }
     }
   }
 }
