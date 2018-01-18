5e6a9fcec92a79434eb066c7c95a0629348854ef
Laurynas <laurynaslavre@gmail.com>
2018-01-18 17:07:04 +0200

    Added button component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,17 +5,22 @@
     </div>
     <div class="aig__view__body">
       {{ msg }}
+      <Button :color="'green'">qwe</Button>
     </div>
   </div>
 </template>
 
 <script>
+import Button from '@/components/Button'
 export default {
   name: 'DashboardView',
   data () {
     return {
       msg: 'Dashboard view'
     }
+  },
+  components: {
+    Button
   }
 }
 </script>
