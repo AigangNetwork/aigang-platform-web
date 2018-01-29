d87ecf5f575620af37e79869a09ba37642c2fe74
Laurynas <laurynaslavre@gmail.com>
2018-01-29 14:05:01 +0200

    after successful login setup interceptor (save authorization header with token for next requests)

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -2,15 +2,26 @@ import * as types from './mutation-types'
 import router from '@/router'
 import { Message } from 'element-ui'
 import i18n from '@/i18n'
+import axios from 'axios'
 
 const logIn = ({ commit }, token) => {
+  // after successful login setup interceptor (save authorization header with token for next requests)
+  axios.interceptors.request.use(config => {
+    config.headers['Authorization'] = `Bearer: ${token}`
+    return config
+  }, error => {
+    return Promise.reject(error)
+  })
+  // commit login mutation
   commit(types.LOGIN, token)
+  // push to / route
+  router.push('/')
+  // push success notification
   Message({
     message: i18n.t('notifications.login_success'),
     type: 'success',
     showClose: true
   })
-  router.push('/')
 }
 
 export { logIn }
