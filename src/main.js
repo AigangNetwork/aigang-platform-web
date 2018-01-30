8afb58af64ad6f50be2fe0457000987003cdf894
Laurynas <laurynaslavre@gmail.com>
2018-01-30 20:17:02 +0200

    added data item description output

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 import Vue from 'vue'
-import App from '@/App'
-import router from '@/router'
-import i18n from '@/i18n'
-
-import store from '@/vuex'
+import VueMoment from 'vue-moment'
 
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import ElementUI from 'element-ui'
+
+import App from '@/App'
+import i18n from '@/i18n'
+import store from '@/vuex'
+import router from '@/router'
 
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/scss/element-aig.scss'
 
 Vue.use(VueAxios, axios)
 Vue.use(ElementUI)
+Vue.use(VueMoment)
 
 Vue.axios.defaults.baseURL = 'http://aigangplatformapi.azurewebsites.net/api'
 Vue.config.productionTip = false
