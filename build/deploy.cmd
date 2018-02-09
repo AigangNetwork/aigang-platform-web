511e1166612ecee7ae66ffe8fe7fd6ee1db91bf7
Marius Adašiūnas <marius.adasiunas@gmail.com>
2018-02-09 13:41:57 +0200

    fixing xcopy command

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -4,7 +4,7 @@ call npm run build
 call cd %DEPLOYMENT_TARGET%
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 
-xcopy /d %DEPLOYMENT_TARGET%\dist\* %DEPLOYMENT_TARGET\dist /s /i
-xcopy /d %DEPLOYMENT_TARGET%\package.json %DEPLOYMENT_TARGET\package.json*
+xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET\dist /s /i
+xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET\package.json*
 
 echo Deployed
