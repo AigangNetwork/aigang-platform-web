a6e548626a19db8f059397dc4d99712f6cae38b2
Marius Adašiūnas <marius.adasiunas@gmail.com>
2018-02-09 12:52:21 +0200

    adding deployement script, testing if it syncs with azure

---
+++ build/deploy.cmd
@@ -0,0 +1,11 @@
+call npm install
+call npm run build
+
+call cd %DEPLOYMENT_TARGET%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+xcopy /d %DEPLOYMENT_TARGET%\dist\* %DEPLOYMENT_TARGET\dist /s /i
+xcopy /d %DEPLOYMENT_TARGET%\package.json %DEPLOYMENT_TARGET\package.json*
+
+call npm install --only=production
+echo Deployed
