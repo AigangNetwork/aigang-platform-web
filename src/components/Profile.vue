4a4afdc4ef458d838634c84987f4ab4ced1c511f
Marius Adašiūnas <marius.adasiunas@gmail.com>
2018-02-26 14:51:04 +0200

    improving navigation bar

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,11 +1,11 @@
 <template>
   <router-link to="/profile" class="aig__profile">
-    <div class="information">
-      {{ $t('account.balance') }}
-      <div class="balance">10 000 AIX</div>
-    </div>
     <div class="photo">
       {{ $store.state.user.profile.firstName.charAt(0) }}
+    </div>
+    <div class="information">
+      {{ $store.state.user.profile.firstName }}
+      <div class="balance">10 000 AIX</div>
     </div>
   </router-link>
 </template>
@@ -16,6 +16,8 @@
 
 .aig__profile {
   @include clearfix;
+  margin: 0 auto;
+  justify-content: center;
   font-weight: normal;
   height: 100%;
   flex-wrap: nowrap;
@@ -23,29 +25,30 @@
   font-size: 11px;
   font-family: $font-primary;
   font-weight: 500;
-  height: 50px;
   text-align: right;
   align-items: center;
   font-size: 11px;
   font-weight: 400;
   line-height: 1;
+  width: 100%;
   .photo {
     pointer-events: none;
     flex-shrink: 0;
-    height: 26px;
-    width: 26px;
+    height: 46px;
+    width: 46px;
     border-radius: 50%;
     margin-left: 7px;
-    background: rgba($black, .45);
+    background: #f3f0f0cc;
     display: flex;
     align-items: center;
     justify-content: center;
     color: rgba(white, .25);
     text-align: center;
+    margin-right: 17px;
   }
   .information {
-    color: $blue-light;
-    text-transform: uppercase;
+    color: $white;
+    margin-right: 10px;
     .balance {
       flex-basis: 100%;
       margin-top: 2px;
