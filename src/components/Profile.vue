430963b2dd4ab7d7e1522b895f31b7e7171b54b1
Marius Adašiūnas <marius.adasiunas@gmail.com>
2018-02-27 09:56:43 +0200

    refactor for navigation

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -16,8 +16,7 @@
 
 .aig__profile {
   @include clearfix;
-  margin: 0 auto;
-  justify-content: center;
+  float: right;
   font-weight: normal;
   height: 100%;
   flex-wrap: nowrap;
@@ -27,12 +26,9 @@
   font-weight: 500;
   text-align: right;
   align-items: center;
-  font-size: 11px;
-  font-weight: 400;
-  line-height: 1;
   width: 100%;
   .photo {
-    pointer-events: none;
+    // pointer-events: none;
     flex-shrink: 0;
     height: 46px;
     width: 46px;
@@ -42,7 +38,6 @@
     display: flex;
     align-items: center;
     justify-content: center;
-    color: rgba(white, .25);
     text-align: center;
     margin-right: 17px;
   }
--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -64,9 +64,7 @@ export default {
 @import '~helpers/mixins';
 
 .aig__profile__wrapper {
-  float: right;
-  width: 10%;
-  height: 100%;
+  width: 200px;
   line-height: 1.4;
 }
 .aig__bar__active {
@@ -82,46 +80,35 @@ export default {
   padding: 10px;
   margin-left: 15px;
   img {
-    display: inline-block;
     vertical-align: middle;
-    height: 25px;
-    width: auto;
+    height: 30px;
   }
 }
 
 .aig__navigation {
   margin: 0 auto;
-  text-align: right;
   max-width: 1520px;
   width: 100%;
   line-height: 50px;
-  top: 0;
-  left: 0;
   .aig__navigation__body {
     @include clearfix;
     font-size: 0;
     background: $purple;
     height: 70px;
-    color: white;
+    display: flex;
     .aig--login {
-      display: inline-block;
-      vertical-align: middle;
-      margin-top: 15px;
+      height: 40px;
+      margin-top: 13px;
       margin-right: 15px;
     }
   }
   .aig__logo {
-    @include transition(all, 100ms, ease);
-    transform-origin: center;
     &:hover {
       opacity: 0.65;
     }
     &:active {
       opacity: 1;
     }
-  }
-  .aig__profile {
-    float: right;
   }
   @include breakpoint(max-width 768px) {
     .aig__navigation__body {
@@ -135,44 +122,28 @@ export default {
 }
 
 .aig__navigation__menu {
-  float: left;
-  height: 100%;
-  width: 80%;
+  width: 100%;
   ul {
     display: flex;
     height: 100%;
-    width: auto;
-    display: flex;
     justify-content: center;
     li {
-      display: inline-flex;
       &+li {
         margin-left: 25px;
       }
       a {
         padding: 15px;
         @include transition(all, 100ms, ease-in-out);
-        user-select: none;
         font-family: $font-primary;
         display: flex;
         align-items: center;
-        position: relative;
         height: 100%;
-        color: white;
+        color: $white;
         font-size: 12px;
         font-weight: 500;
         letter-spacing: 2px;
         &:hover {
-          color: rgba(white, .65);
-        }
-        &.router-link-active {
-          pointer-events: none;
-          color: white;
-          .icon {
-            svg {
-              fill: $purple;
-            }
-          }
+          color: rgba($white, .65);
         }
       }
     }
