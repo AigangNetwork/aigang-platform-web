8981db669c4b2b1820e6adad42d21ce76067e1f1
Domantas <domantas.pelaitis@gmail.com>
2018-03-12 17:16:36 +0200

    Added styles and content to ForgotPassword view

--- src/views/guest/ForgotPassword.vue
+++ src/views/guest/ForgotPassword.vue
@@ -1,26 +1,55 @@
 <template>
   <div class="">
-    <div class="aig__container" style="max-width: 400px">
-        <Card>
-          <div slot="body" v-loading="loading" :element-loading-text="$t('general.loading')">
-            <div class="aig__logo">
-              <img src="/static/logo-color.png" alt="">
-            </div>
-            <el-form :model="forgotPasswordForm" :rules="forgotPasswordFormRules" ref="forgotPasswordForm">
-              <el-form-item :label="$t('strings.email')" prop="email" size="small">
-                <el-input v-model="forgotPasswordForm.email" size="small" placeholder="example@aigang.network"></el-input>
-              </el-form-item>
-              <el-form-item style="margin-bottom: 0">
-                <el-button type="primary" @click="submitForm('forgotPasswordForm')" style="width: 100%">{{ $t('actions.retrievePassword') }}</el-button>
-              </el-form-item>
-            </el-form>
+    <div class="aig__container">
+        <Card style="width: 932px;" >
+
+          <div style="height:530px;" class="flex_wrap" slot="body" v-loading="loading" :element-loading-text="$t('general.loading')">
+
+            <el-row>
+              <el-col>
+                <div class="aig__logo">
+                  <img src="/static/logo-purple.png" alt="">
+                </div>
+              </el-col>
+            </el-row>
+
+            <el-row type="flex" justify="center" style="min-height: 350px;">
+              <el-col :span="10" >
+                <h2 style="margin-top:40px;margin-bottom:40px;">{{ $t('retrievePassword.title') }}</h2>
+                <el-form :model="forgotPasswordForm" :rules="forgotPasswordFormRules" ref="forgotPasswordForm">
+                  <el-row>
+                    <el-col><span class="label">{{ $t('login.email' )}}</span></el-col>
+                  </el-row>
+
+                  <el-row>
+                    <el-col>
+                      <el-form-item prop="email">
+                        <el-input v-model="forgotPasswordForm.email" size="small" placeholder="example@aigang.network"></el-input>
+                      </el-form-item>
+                    </el-col>
+                  </el-row>
+
+                  <el-form-item style="margin-bottom: 0">
+                    <el-button type="primary" @click="submitForm('forgotPasswordForm')" style="width: 100%">{{ $t('actions.retrievePassword') }}</el-button>
+                  </el-form-item>
+                </el-form>
+              </el-col>
+            </el-row>
+
+            <el-row type="flex" justify="center" style="height:100px;align-items: flex-end; flex-grow: 1;">
+              <el-col :span="10" style="text-align: center;">
+                <span>{{ $t('retrievePassword.backTo') }}</span>
+                 <router-link class="a--active" to="/login">{{ $t('login.login') }}</router-link>
+              </el-col>
+            </el-row>
+
           </div>
+
           <div slot="footer">
             <router-link to="/register">{{ $t('actions.createAccount') }}</router-link>
             <router-link to="/login">{{ $t('actions.loginToAccount') }}</router-link>
           </div>
         </Card>
-        <!-- </div> -->
     </div>
   </div>
 </template>
@@ -41,8 +70,16 @@ export default {
       },
       forgotPasswordFormRules: {
         email: [
-          { required: true, message: 'Please input email address', trigger: 'blur' },
-          { type: 'email', message: 'Please input correct email address', trigger: 'blur, change' }
+          {
+            required: true,
+            message: 'Please enter email address',
+            trigger: 'blur'
+          },
+          {
+            type: 'email',
+            message: 'Please enter correct email address',
+            trigger: 'blur, change'
+          }
         ]
       }
     }
@@ -59,13 +96,16 @@ export default {
     },
     retrievePassword () {
       this.loading = true
-      this.axios.post('/account/forgotpassword', this.forgotPasswordForm).then(response => {
-        this.loading = false
-        console.log(response)
-      }).catch(error => {
-        this.loading = false
-        console.log(error)
-      })
+      this.axios
+        .post('/account/forgotpassword', this.forgotPasswordForm)
+        .then(response => {
+          this.loading = false
+          console.log(response)
+        })
+        .catch(error => {
+          this.loading = false
+          console.log(error)
+        })
     }
   }
 }
@@ -77,16 +117,27 @@ export default {
 
 .aig__logo {
   display: block;
-  line-height: 50px;
-  font-size: 0;
-  margin-right: 40px;
-  text-align: center;
-  margin-bottom: 15px;
+  line-height: 80px;
   img {
     display: inline-block;
     vertical-align: middle;
-    height: 36px;
+    width: 86px;
+    height: 27px;
     width: auto;
   }
+}
+
+.aig__container {
+  max-width: 1200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 700px;
+  background-image: url('/static/background/backgroud_pattern.svg');
+}
+
+.flex_wrap {
+  display: flex;
+  flex-direction: column;
 }
 </style>
