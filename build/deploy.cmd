a671b3d0230e0bc26950714f78269cc27fb4342c
DarDev <darius.e.pastas@gmail.com>
2018-03-26 12:32:38 +0300

    update deploy cmd

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -6,5 +6,6 @@ for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 
 xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET% /s /i
 xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json*
+xcopy /d %DEPLOYMENT_SOURCE%\web.config %DEPLOYMENT_TARGET%\web.config*
 
 echo Deployed
