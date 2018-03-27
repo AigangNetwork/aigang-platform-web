cd3113f1c686358f31d1928cfe5c79e21825741a
DarDev <darius.e.pastas@gmail.com>
2018-03-27 11:22:05 +0300

    added npm i win-node-env to run commands as in linux

--- build/config/dev.env.js
+++ build/config/dev.env.js
@@ -1,6 +1,6 @@
 'use strict'
 const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
+const prodEnv = require('./production.env.js')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"'
