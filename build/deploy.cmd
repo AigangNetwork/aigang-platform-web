358f1bad34a6b70261d6f7f204dfdb01216b38c5
DarDev <darius.e.pastas@gmail.com>
2018-03-27 13:12:16 +0300

    fix directory cleaning

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -17,16 +17,20 @@ IF "%NODE_ENV%" == "qa" (
 echo [custom] build succeded
 call cd %DEPLOYMENT_TARGET%
 REM first the directories /ad option of dir
-for /F "delims=" %%i in ('dir /b /ad') do (echo rmdir "%%i" /s/q)
+REM for /F "delims=" %%i in ('dir /b /ad') do (echo rmdir "%%i" /s/q)
 REM now the files /a-d option of dir
-for /F "delims=" %%i in ('dir /b /a-d') do (echo del "%%i" /q)
+REM for /F "delims=" %%i in ('dir /b /a-d') do (echo del "%%i" /q)
 
+IF EXIST %DEPLOYMENT_TARGET% (
+    FOR /D %%p IN ("%DEPLOYMENT_TARGET%\*.*") DO rmdir "%%p" /S /Q
+    del%DEPLOYMENT_TARGET%\*.* /F /Q
+)
 
 echo [custom] deployment_source %DEPLOYMENT_SOURCE%
 echo "[custom] copy dist folder" 
-xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET% /s /i
+xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET% /s /i /Y
 
 echo "[custom] copy web.config" 
-xcopy /d %DEPLOYMENT_SOURCE%\web.config %DEPLOYMENT_TARGET%\web.config*
+xcopy /d %DEPLOYMENT_SOURCE%\web.config %DEPLOYMENT_TARGET%\web.config* /Y
 
 echo [custom] Deployed
