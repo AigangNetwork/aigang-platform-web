be6ad23dc9daccafab058775a5996400f316fa2f
zilvinas <ziljuc@gmail.com>
2018-11-27 11:48:14 +0200

    Available refund type label changed

--- src/views/pools/PoolContributionLabel.vue
+++ src/views/pools/PoolContributionLabel.vue
@@ -6,7 +6,7 @@
     <el-tag v-else-if="type === 'availablePayout'" type="info" :size="size">{{ $t('pools.poolContributionStatuses.availablePayout') }}</el-tag>
     <el-tag v-else-if="type === 'pendingPayout'" type="info" :size="size">{{ $t('pools.poolContributionStatuses.pendingPayout') }}</el-tag>
     <el-tag v-else-if="type === 'rewardPaidout'" type="success" :size="size">{{ $t('pools.poolContributionStatuses.rewardPaidout') }}</el-tag>
-    <el-tag v-else-if="type === 'availableRefund'" type="danger" :size="size">{{ $t('pools.poolContributionStatuses.availableRefund') }}</el-tag>
+    <el-tag v-else-if="type === 'availableRefund'" type="warning" :size="size">{{ $t('pools.poolContributionStatuses.availableRefund') }}</el-tag>
     <el-tag v-else-if="type === 'refundPaidout'" type="success" :size="size">{{ $t('pools.poolContributionStatuses.refundPaidout') }}</el-tag>
   </span>
 </template>
