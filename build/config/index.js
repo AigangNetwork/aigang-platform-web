65756d0c4634c18c19c2323862f89db34a559036
zilvinas <ziljuc@gmail.com>
2018-12-06 15:23:29 +0200

    Optimisations for production build

--- build/config/index.js
+++ build/config/index.js
@@ -55,7 +55,7 @@ module.exports = {
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
