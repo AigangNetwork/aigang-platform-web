7c3faa5a51b3a090913f3b6017b9fb6f03024c9f
Domantas Pelaitis <domantas.pelaitis@gmail.com>
2019-01-07 11:31:40 +0200

    Fixes #162

--- src/components/pools/ContributionStatus.vue
+++ src/components/pools/ContributionStatus.vue
@@ -1,7 +1,10 @@
 <template>
   <span v-if="status === 'pendingPayment'"> {{ $t('pools.poolContributionStatuses.pendingPayment') }} </span>
   <span v-else-if="status === 'paid'"> {{ $t('pools.poolContributionStatuses.paid') }} </span>
-  <span v-else-if="status === 'canceled'"> {{ $t('pools.poolContributionStatuses.canceled') }} </span>
+  <span v-else-if="status === 'canceled'">
+    {{ $t('pools.poolContributionStatuses.canceled') }}
+    <el-tooltip :content="$t('pools.contribution.explanations.canceled')"> <i class="el-icon-info"></i> </el-tooltip>
+  </span>
   <span v-else-if="status === 'availablePayout'"> {{ $t('pools.poolContributionStatuses.availablePayout') }} </span>
   <span v-else-if="status === 'pendingPayout'"> {{ $t('pools.poolContributionStatuses.pendingPayout') }} </span>
   <span v-else-if="status === 'rewardPaidout'"> {{ $t('pools.poolContributionStatuses.rewardPaidout') }} </span>
