c3867fe23c485fbf2897e881000835ebd6ec8dcd
Domantas Pelaitis <domantas.pelaitis@gmail.com>
2019-04-03 15:01:19 +0300

    Added insurance product limits validation

--- src/domain/InsuranceProduct.js
+++ src/domain/InsuranceProduct.js
@@ -6,25 +6,43 @@ export default class InsuranceProduct {
     this.address = details.address
     this.premiumCalculatorAddress = details[0]
     this.investorsPoolAddress = details[1]
-    this.startDateUtc = moment.unix(details[2]).utc().format('YYYY-MM-DD HH:mm')
-    this.endDateUtc = moment.unix(details[3]).utc().format('YYYY-MM-DD HH:mm')
+    this.startDateUtc = moment
+      .unix(details[2])
+      .utc()
+      .format('YYYY-MM-DD HH:mm')
+    this.endDateUtc = moment
+      .unix(details[3])
+      .utc()
+      .format('YYYY-MM-DD HH:mm')
     this.title = details[4]
     this.description = details[5]
     this.policyTermInDays = Math.floor(parseInt(details[6]) / (60 * 60 * 24))
     this.basePremium = EthUtils.fromWei(details[7])
     this.payout = EthUtils.fromWei(details[8])
     this.fee = details[9]
-    this.policiesLimit = details[10]
+
     this.productPoolLimit = details[11]
-    this.createdUtc = moment.unix(details[12]).utc().format('YYYY-MM-DD HH:mm')
+    this.createdUtc = moment
+      .unix(details[12])
+      .utc()
+      .format('YYYY-MM-DD HH:mm')
     this.type = details.type
+
+    const policiesLimit = parseInt(details[10])
+    this.isPoliciesLimitReached = policiesLimit <= details.policiesCount
+
+    const poolLimit = parseInt(details[11])
+    this.isPoolLimitReached = poolLimit <= details.poolSize
   }
 
   static async create (contract, type) {
     const details = await contract.methods.getProductDetails().call()
+    const stats = await contract.methods.getProductStats().call()
 
     details.address = contract._address
     details.type = type
+    details.policiesCount = parseInt(stats[2])
+    details.poolSize = parseInt(stats[1])
 
     const product = new InsuranceProduct()
     await product.initialize(details)
